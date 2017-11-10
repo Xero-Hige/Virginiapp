c738ab7064f68b0db87057fd6524525b82b6e196
Gaston Alberto Martinez <Xero-Hige@users.noreply.github.com>
2017-11-10 19:32:04 -0300

    Add files via upload

---
+++ server.py
@@ -0,0 +1,18 @@
+import os
+import random
+import re
+from subprocess import PIPE, Popen
+
+from flask import Flask, redirect, render_template, request
+
+app = Flask(__name__)
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+
+
+@app.route('/', methods=["GET"])
+def root():
+    return render_template("template.html")
+
+@app.route('/', methods=["POST"])
+def add():
+    return redirect("/")
